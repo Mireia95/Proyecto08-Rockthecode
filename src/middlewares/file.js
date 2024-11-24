a2e52338f2b1deedd9de511f05c247d95b04654e
Mireia <mireia95.granzotto@gmail.com>
2024-11-24 18:24:33 +0100

    feedback prof req.query
    añadidos comentarios sobre como seria el mejor codigo para pasarle la carpeta a cloudinary segun la coleccion que estamos subiendo

--- src/middlewares/file.js
+++ src/middlewares/file.js
@@ -9,14 +9,17 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  /*gracis al middleware verifyCollection, he pasado a req req.collection, que dependerà de que colección es el dato que estoy pasando
-   a la folder de Cloudinary le paso este dato para asegurarme donde va guardada la imagen.
+  /*gracis al middleware verifyCollection, he pasado a req req.collection, que dependerà de que colección es el dato que estoy pasando, a la folder de Cloudinary le paso este dato para asegurarme donde va guardada la imagen.
    Lo he conseguido informandome de lo que podia aceptar "params". Una vez descubierto que podía aceptar una funcion y pasarle req, he decidido pasarle req.collection y decidir asi el nombre de la folder
   */
+  //! SOLUCION PROFE: El req.query es una parte crucial del request object (objeto de solicitud) en Express.js. Este objeto contiene toda la información que llega al servidor desde el cliente, incluyendo los parámetros de la URL. Por ejemplo, si tienes una ruta que muestra detalles de un producto y la URL es https://tusitio.com/producto?id=123, el req.query contendrá { id: '123' }, lo que te permite acceder al valor del parámetro id y tomar acciones en función de él.
+  //!en este caso a "folder" le pasaríamos req.query
+  //*https://keepcoding.io/blog/que-significa-el-req-query-en-express-js/
   params: async (req, file) => {
     console.log(req.collection);
     return {
-      folder: `RTCProyecto08_${req.collection}`,
+      //para guardar SUBCARPETAS utilizar /
+      folder: `RTCProyecto08/${req.collection}`,
       allowedFormats: ['jpg', 'png', 'jpeg', 'gif', 'webp']
     };
   }
